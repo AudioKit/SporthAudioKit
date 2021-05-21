364b9a73a41bf9777440d29577d12b109d8e58a0
Aurelius Prochazka <aurephd@gmail.com>
2021-05-20 19:55:36 -0700

    Added Github stuff including CI

---
+++ Sources/CSporthAudioKit/include/CSporthAudioKit.h
@@ -0,0 +1,8 @@
+// Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/AudioKit/
+
+#ifndef CSporthAudioKit_h
+#define CSporthAudioKit_h
+
+// CSporthAudioKit has no public interface. It registers DSP kernels automatically.
+
+#endif /* CSporthAudioKit_h */
