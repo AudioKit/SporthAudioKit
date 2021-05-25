ae3f49c82f5404c18e37e0b4bf8068c0157ccf1f
Aurelius Prochazka <aurephd@gmail.com>
2021-05-24 17:39:26 -0700

    Updated dependencies

--- Package.swift
+++ Package.swift
@@ -16,13 +16,12 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/AudioKit/Soundpipe", .branch("main")),
         .package(url: "https://github.com/AudioKit/SoundpipeAudioKit", .branch("develop")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(name: "Sporth", dependencies: ["Soundpipe"]),
+        .target(name: "Sporth", dependencies: ["SoundpipeAudioKit"]),
         .target(
             name: "SporthAudioKit",
             dependencies: ["SoundpipeAudioKit", "CSporthAudioKit", "Sporth"]),
