992f782736c7f3f421f918ec6711f48656201211
Aurelius Prochazka <aurephd@gmail.com>
2021-12-22 16:33:44 -0800

    Updated to AK 5.3

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "SporthAudioKit",
     platforms: [.macOS(.v10_13), .iOS(.v11), .tvOS(.v11)],
     products: [.library(name: "SporthAudioKit", targets: ["SporthAudioKit"])],
-    dependencies: [.package(url: "https://github.com/AudioKit/SoundpipeAudioKit", from: "5.2.0")],
+    dependencies: [.package(url: "https://github.com/AudioKit/SoundpipeAudioKit", from: "5.3.0")],
     targets: [
         .target(name: "Sporth", dependencies: ["SoundpipeAudioKit"]),
         .target(name: "SporthAudioKit", dependencies: ["SoundpipeAudioKit", "CSporthAudioKit", "Sporth"]),
